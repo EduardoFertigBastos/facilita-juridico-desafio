30ec9535e9c5abd38e699a6a49cf9250e3a06beb
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-03-06 20:50:38 -0300

    general

---
+++ src/styles/MUITheme.ts
@@ -0,0 +1,19 @@
+'use client';
+import { green } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+const MUITheme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3',
+    },
+    secondary: {
+      main: '#4dabf5',
+    },
+    success: {
+      main: green['400'],
+    },
+  },
+});
+
+export default MUITheme;
