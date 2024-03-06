46ca02383e98088f58c1559b0770ae0cad99042f
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-03-06 20:51:06 -0300

    tooltip

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,16 @@
+import React, { PropsWithChildren } from 'react';
+import { Container } from './styles';
+
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<PropsWithChildren<TooltipProps>> = ({ title, className, children }) => (
+  <Container className={className}>
+    {children}
+    <span>{title}</span>
+  </Container>
+);
+
+export default Tooltip;
